691d33a089c19170f9da826479457dc7e123cd3e
Pokai Chang <mail@zeta.vg>
2024-07-14 23:09:10 +0800

    [config]: move config related types into `src/config`

---
+++ packages/vxrn/types/config/types.d.ts
@@ -0,0 +1,26 @@
+import type { Hono } from 'hono';
+import type { Options as FlowOptions } from '@vxrn/vite-flow';
+import type { AfterBuildProps } from '../types';
+export type VXRNOptions = {
+    /**
+     * The entry points to your app. For web, it defaults to using your `root` to look for an index.html
+     *
+     * Defaults:
+     *   native: ./src/entry-native.tsx
+     */
+    entries?: {
+        native?: string;
+        web?: string;
+    };
+    root?: string;
+    host?: string;
+    port?: number;
+    /**
+     * Uses mkcert to create a self-signed certificate
+     */
+    https?: boolean;
+    flow?: FlowOptions;
+    afterBuild?: (props: AfterBuildProps) => void | Promise<void>;
+    afterServerStart?: (options: VXRNOptions, app: Hono) => void | Promise<void>;
+};
+//# sourceMappingURL=types.d.ts.map
